2d42386fa21b90d01d6501c2ef4a9c46b6a116c5
Daijun <zijun2030@163.com>
2024-10-19 14:19:58 +0800

    🔧 config(eslint): 配置 ESLint 以支持 UnoCSS 并调整相关规则
    - 在 eslint.config.js 中添加 UnoCSS 支持
    - 调整 ESLint 规则，允许使用 console.log
    - 在 package.json 中添加 @unocss/eslint-plugin 依赖
    - 更新 pnpm-lock.yaml 以反映新添加的依赖

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,12 @@ export default jun({
   vue: true,
   typescript: true,
   formatters: true,
+  unocss: true,
+  rules: {
+    // 禁止使用尾随逗号
+    // 'style/comma-dangle': [2, 'never'],
+    // 允许使用consoles
+    'no-console': 0,
+  },
   ignores: ['**/dist/**', '**/node_modules/**'],
 })
